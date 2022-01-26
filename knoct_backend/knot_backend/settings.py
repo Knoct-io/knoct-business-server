dbb49d0426895d29c8258abbc22083066497de17
Shaishav Gupta <shaishav.gupta@spinny.com>
2022-01-27 02:02:08 +0530

    Allowed Hosts

--- knoct_backend/knot_backend/settings.py
+++ knoct_backend/knot_backend/settings.py
@@ -139,9 +139,9 @@ INTERNAL_IPS = [
     "127.0.0.1",
 ]
 
-# CORS_ALLOWED_ORIGINS = [
-#     'http://127.0.0.1:3000',
-#     'http://localhost:3000'
-# ]
+CORS_ALLOWED_ORIGINS = [
+    'http://127.0.0.1:3000',
+    'http://localhost:3000'
+]
 
 CORS_ALLOW_HEADERS = list(default_headers) + []
